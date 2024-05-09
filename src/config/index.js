d0d3a70aa2625a55426bb24f3211cafb11ffec25
Leonardo Amaro <=>
2024-05-09 17:10:45 -0300

    trabalhando no retorno do audio

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-// export const baseURL = `https://devices.wavoip.com`;
+export const baseURL = `https://devices.wavoip.com`;
 
-export const baseURL = `http://localhost:9000`;
+// export const baseURL = `http://localhost:9000`;
